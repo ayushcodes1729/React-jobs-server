a93f7ff4d5ab0aa215ccaa336f04c88507caa64f
Ayush Kumar <ayushtiwari0803@gmail.com>
2025-02-14 00:41:14 +0530

    CORS updated

--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
 const express = require("express");
 const connectDB = require("./config/database");
 require("dotenv").config();
-const cors = require("cors")
+const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.use(cors({
-    origin : "https://react-jobs-steel.vercel.app/",
-    credentials : true
-}))
-app.use(express.json())
+app.use(
+  cors({
+    origin: "https://react-jobs-steel.vercel.app", // Replace with your frontend URL
+    methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
+    credentials: true, // If using cookies or authentication headers
+  })
+);
+app.use(express.json());
 
-const jobRouter = require('./routes/job')
+const jobRouter = require("./routes/job");
 
-app.use('/', jobRouter);
+app.use("/", jobRouter);
 // app.get('/',(req,res)=>{
 //     console.log("Hello World");
 //     res.send("Hello World")
 // })
 
 connectDB()
-    .then(() => {
-        console.log("Database Connection established...");
-        app.listen(PORT, () => {
-            console.log("Server listening successfully on port", PORT + "...");
-        });
-    })
-    .catch((err) => {
-        console.error("Database cannot be connected");
+  .then(() => {
+    console.log("Database Connection established...");
+    app.listen(PORT, () => {
+      console.log("Server listening successfully on port", PORT + "...");
     });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected");
+  });
